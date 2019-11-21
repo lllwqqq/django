a8aab48b1bea43efbaddb2514ebb9a457616d39e
leewq <lllwqqq@163.com>
2019-11-21 23:20:20 +0800

    1.完成首页首页渲染 2.完成创建时间、修改时间、作者、admin后台文章列表及添加文章字段优化

--- test01/test01_blog/blog/models.py
+++ test01/test01_blog/blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    name = models.CharField(max_length=100,verbose_name='分类')
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100, blank=True,verbose_name='标签')
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    title = models.CharField(max_length=100, verbose_name='标题')
+    excerpt = models.CharField(max_length=200, verbose_name='摘要')
+    body = models.TextField(verbose_name='正文')
+    create_time = models.DateField(default=timezone.now, verbose_name='创建时间')
+    modified_time = models.DateField(verbose_name='修改时间')
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='分类')
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='标签')
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
+
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
--- test01/test01_blog/blog/views.py
+++ test01/test01_blog/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    post_list = Post.objects.all().order_by('-create_time')
+    return render(request,template_name='blog/index.html',context={
+        'post_list': post_list
+    })
