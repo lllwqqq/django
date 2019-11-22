b972f3b30f804f117fb923f977b7031add06d287
leewq <lllwqqq@163.com>
2019-11-22 18:24:01 +0800

    1.完成文章支持markdown语法 2.完成有无目录判断显示（1.我这里是在前端判断的 2.可以在后端判断，后面优化）

--- test01/test01_blog/blog/views.py
+++ test01/test01_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+import markdown
 
 # Create your views here.
 def index(request):
@@ -9,6 +10,15 @@ def index(request):
     })
 def detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
+    md = markdown.Markdown(
+        extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',]
+    )
+    post.body = md.convert(post.body)
+    post.toc = md.toc
+
     return render(request,template_name='blog/detail.html',context={
         'post': post
     })
