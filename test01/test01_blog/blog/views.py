cbfbc4deb27936fc1c418b1daa86b2ab5d2cfe38
leewq <lllwqqq@163.com>
2019-11-22 19:03:49 +0800

    1. 完成后端用正则处理空目录

--- test01/test01_blog/blog/views.py
+++ test01/test01_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 import markdown
+import re
 
 # Create your views here.
 def index(request):
@@ -17,7 +18,8 @@ def detail(request,pk):
             'markdown.extensions.toc',]
     )
     post.body = md.convert(post.body)
-    post.toc = md.toc
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>',md.toc,re.S)
+    post.toc = m.group(1) if m is not None else ''
 
     return render(request,template_name='blog/detail.html',context={
         'post': post
