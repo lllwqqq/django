d8c5137376db7a12e95911c059545207cff304aa
liwq <liwq@spacesforce.com>
2019-12-01 10:07:03 +0800

    init project

--- test02/test02_blog/blog/urls.py
+++ test02/test02_blog/blog/urls.py
@@ -14,5 +14,8 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:pk>',views.detail,name='detail')
+    path('posts/<int:pk>',views.detail,name='detail'),
+    path('archives/<int:year>/<int:month>/', views.archives, name='archives'),
+    path('categories/<int:pk>/', views.category, name='category'),
+    path('tags/<int:pk>/', views.tags, name='tags')
 ]
--- test02/test02_blog/blog/views.py
+++ test02/test02_blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post,Category,Tag
 import markdown
 import re
 
@@ -24,4 +24,27 @@ def detail(request, pk):
     post.toc = m.group(1) if m is not None else ''
     return render(request, template_name='blog/detail.html', context={
         'post': post
+    })
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(create_time__year=year,
+                                    create_time__month=month).order_by('-create_time')
+    return render(request, template_name='blog/index.html', context={
+        'post_list': post_list
+    })
+
+
+def category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=category).order_by('-create_time')
+    return render(request, template_name='blog/index.html', context={
+        'post_list': post_list
+    })
+
+
+def tags(request, pk):
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t).order_by('-create_time')
+    return render(request, template_name='blog/index.html', context={
+        'post_list': post_list
     })
