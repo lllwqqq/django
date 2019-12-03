22d7fc2f09d24ea2f3dfbc8aa0e47e511c817bdd
liwq <liwq@spacesforce.com>
2019-12-03 19:51:27 +0800

    1. 增加静态文件目录及css\js

---
+++ test03/test03_blog/blog/urls.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Time :    2019/12/1 上午10:15
+@Author:  Aroma
+@File: urls.py
+@Software: PyCharm
+"""
+
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.index, name='index'),
+
+]
+
