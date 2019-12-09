547bbaeeb561fbd0c137afc08ef40af66ed5d380
liwq <liwq@spacesforce.com>
2019-12-09 18:20:27 +0800

    1. 完成归档、分类、标签功能的筛选

--- test03/test03_blog/blog/urls.py
+++ test03/test03_blog/blog/urls.py
@@ -14,7 +14,10 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:pk>/',views.detail,name='detail')
+    path('posts/<int:pk>/',views.detail,name='detail') ,
+    path('archives/<int:year>/<int:month>/',views.archives,name='archives'),
+    path('categories/<int:pk>/',views.categories,name='categories'),
+    path('tags/<int:pk>/',views.tag,name='tag'),
 
 ]
 
