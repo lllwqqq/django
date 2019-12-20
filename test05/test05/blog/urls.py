811e66ed9af4c9343527ff4f1b84ada1d9615c08
liwq <liwq@spacesforce.com>
2019-12-20 16:02:52 +0800

    完成自定义标签的跳转url

--- test05/test05/blog/urls.py
+++ test05/test05/blog/urls.py
@@ -14,4 +14,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:pk>/', views.detail, name='detail'),
+    path('posts/<int:year>/<int:month>/',views.archive,name='archive'),
+    path('posts/category/<int:pk>/',views.categories,name='category'),
+    path('posts/tag/<int:pk>/',views.tag,name='tag'),
 ]
