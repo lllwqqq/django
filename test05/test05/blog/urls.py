7e2d7fa49c363a1258d0c5884152d060b73b1816
liwq <liwq@spacesforce.com>
2019-12-20 15:09:26 +0800

    完成自动生成摘要 完成文章详情页渲染 完成markdown支持 完成自动生成目录

--- test05/test05/blog/urls.py
+++ test05/test05/blog/urls.py
@@ -8,7 +8,10 @@
 @Software: PyCharm
 """
 from django.urls import path
-from . import  views
+from . import views
+
+app_name = 'blog'
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', views.index, name='index'),
+    path('post/<int:pk>/', views.detail, name='detail'),
 ]
